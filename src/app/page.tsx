af8175e39a62c023689a87f9aed8064d4d16b948
Anuj Rawat <rawatanuj07@gmail.com>
2023-12-07 19:07:04 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-<div><h1>hello</h1></div>
+<div><h1>Website Under Maintenance</h1>
+<p>For further queries contact: rawatanuj07@gmail.com</p></div>
+
   )
 }
